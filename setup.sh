dacaa94137de5b4678e4aeae7b7078558bcaaa8f
Luis Valgoi <luis.valgoi@sap.com>
2020-09-19 20:59:34 -0300

    chore: updated setup sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"luisvalgoi@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+
 " > ~/.streamlit/config.toml

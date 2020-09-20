84329aa502871737be54e111e583c1aa227448ce
Luis Valgoi <luis.valgoi@sap.com>
2020-09-19 21:21:09 -0300

    chore: another attempt to deploy

--- create_config.sh
+++ create_config.sh
@@ -1,9 +1,14 @@
-mkdir -p ~/.streamlit
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"luisvalgoi@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
+
 " > ~/.streamlit/config.toml
